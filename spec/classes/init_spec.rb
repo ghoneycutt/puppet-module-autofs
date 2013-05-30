c517b73dd4ed3a01feadd66a41a75d777a3e1b40
Garrett Honeycutt <code@garretthoneycutt.com>
2013-05-30 16:10:08 +0200

    Add spec tests for autofs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 describe 'autofs' do
   describe 'verify_autofs' do
-    let(:facts) { {:osfamily => 'redhat',
-                   :lsbmajdistrelease => '6' } }
+    let(:facts) {
+      {
+        :osfamily          => 'redhat',
+        :lsbmajdistrelease => '6',
+      }
+    }
     let(:title) { 'auto_master' }
-    it { 
+
+    it {
       should include_class('autofs')
+      should include_class('nfs')
+
       should contain_package('autofs').with({
         'ensure' => 'present',
       })
-    }
-    it { 
-      should include_class('autofs')
+
       should contain_file('auto.master').with({
         'ensure'  => 'file',
         'owner'   => 'root',
         'group'   => 'root',
         'mode'    => '0644',
         'path'    => '/etc/auto.master',
-       })
-     }
-    it {
-      should include_class('autofs')
+      })
+
       should contain_service('autofs').with({
         'ensure'     => 'running',
         'enable'     => 'true',
